79c5646094da60e9a7619f0c3200ab9ec464b809
gzock <gzock@example.com>
2019-04-02 00:09:27 +0900

    svgのcontent-typeが正しくないせいでS3経由で見たときにブラウザでレンダリング出来ない問題に対処

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ cd dist/harvest-webapp
 sleep 10
 
 aws s3 sync . s3://product-harvest-webapp --include "*" --cache-control "max-age=3600" --delete --exact-timestamps
+aws s3 cp --recursive s3://product-harvest-webapp s3://product-harvest-webapp --exclude '*' --include '*.svg' --metadata-directive "REPLACE" --content-type "image/svg+xml"
 aws cloudfront create-invalidation --distribution-id E190IJHVIC9O74 --paths '/*'

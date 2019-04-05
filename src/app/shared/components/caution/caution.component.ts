596a2be4f1a27dfffe03ed094cab404334eed3df
gzock <gzock@example.com>
2019-04-05 09:32:18 +0000

    コンストラクタの中身を掃除(使っていないので)

--- src/app/shared/components/caution/caution.component.ts
+++ src/app/shared/components/caution/caution.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
-
 @Component({
   selector: 'app-caution',
   templateUrl: './caution.component.html',
@@ -10,7 +9,6 @@ import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 })
 export class CautionComponent implements OnInit {
   constructor(
-    public dialogRef: MatDialogRef<ConfirmDeleteComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 

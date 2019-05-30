0e8345d9d9d502d913a4ec8240d643c2fd55a3bb
gzock <gzock@example.com>
2019-05-30 11:07:29 +0000

    プロジェクトコードをクリップボードにコピーする機能を実装

--- src/app/shared/components/setting-project/setting-project.component.ts
+++ src/app/shared/components/setting-project/setting-project.component.ts
@@ -118,7 +118,22 @@ export class SettingProjectComponent implements OnInit {
      );
   }
 
-  private openSucccessAlert(msg) {
+  public onCopyClipboard(str: string) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = str;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.openSuccessAlert("クリップボードにコピーしました。");
+  }
+
+  private openSuccessAlert(msg) {
     this.alertService.openSucccessAlert(msg);
   }
 

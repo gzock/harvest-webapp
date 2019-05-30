11a13f843d268bc575c341d9f53694b35ba4381e
gzock <gzock@example.com>
2019-05-30 05:24:09 +0000

    メンバー管理用にAPIのレスポンスのプロパティが増えたのでインターフェースとしても追加

--- src/app/private/dashboard/projects/project.ts
+++ src/app/private/dashboard/projects/project.ts
@@ -1,9 +1,10 @@
-// export class 
 export interface Project {
   project_id: string;
+  project_code: string;
   name: string;
   created_at: string;
   updated_at: string;
   start_on: string;
   complete_on: string;
+  role: string;
 }

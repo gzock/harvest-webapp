393166f1259330636bdd29b613b36864962967a1
gzock <gzock@example.com>
2019-07-05 22:41:52 +0900

    ServiceWorkerの古いアプリのキャッシング対策

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'harvest-webapp';
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }

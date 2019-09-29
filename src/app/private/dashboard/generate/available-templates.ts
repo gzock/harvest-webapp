8a1013f89cfcb624d883bdd98a9da11a84c0f551
gzock <gzock@example.com>
2019-09-29 08:09:07 +0000

    インターフェースとして独立

---
+++ src/app/private/dashboard/generate/available-templates.ts
@@ -0,0 +1,8 @@
+import { TemplateConfig } from './template-config';
+
+export interface AvailableTemplates {
+  default: TemplateConfig[];
+  user: TemplateConfig[];
+  project: TemplateConfig[];
+}
+
